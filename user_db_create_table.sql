1d05d8193371186872ded7498fd7160e0a933a28
Sanchit Bansal <sanchitbansal10@gmail.com>
2023-03-24 11:57:02 +0530

    setup scripts for user db, tweetdb create and seed

---
+++ user_db_create_table.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+   id VARCHAR(255) PRIMARY KEY,
+   username VARCHAR(255),
+   email VARCHAR(255),
+   password VARCHAR(255),
+   first_name VARCHAR(255),
+   last_name VARCHAR(255),
+   bio TEXT,
+   profile_picture VARCHAR(255),
+   created_at datetime,
+   updated_at datetime
+);
---
+++ user_db_seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (id, username, email, password, first_name, last_name, bio, profile_picture, created_at, updated_at)
+VALUES 
+('1', 'johndoe', 'johndoe@example.com', 'password123', 'John', 'Doe', 'I am a software engineer', 'https://example.com/profile_picture.jpg', 
+'2020-01-01 00:00:00', '2020-01-01 00:00:00'),
+(2, 'janedoe', 'janedoe@example.com', 'password456', 'Jane', 'Doe', 'I am a graphic designer', 'https://example.com/profile_picture.jpg', '2020-01-01 
+00:00:00', '2020-01-01 00:00:00'),
+(3, 'bobsmith', 'bobsmith@example.com', 'password789', 'Bob', 'Smith', 'I am a writer', 'https://example.com/profile_picture.jpg','2020-01-01 
+00:00:00', '2020-01-01 00:00:00');
+
