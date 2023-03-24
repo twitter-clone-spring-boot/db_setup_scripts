062e5759b4e6ca62aacaef5dc0ef2302954f7ff7
Sanchit Bansal <sanchitbansal10@gmail.com>
2023-03-24 12:00:18 +0530

    TC-21: make email unique and indexed

--- user_db_create_table.sql
+++ user_db_create_table.sql
@@ -3,12 +3,13 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
    id VARCHAR(255) PRIMARY KEY,
    username VARCHAR(255),
-   email VARCHAR(255),
+   email VARCHAR(255) UNIQUE,
    password VARCHAR(255),
    first_name VARCHAR(255),
    last_name VARCHAR(255),
    bio TEXT,
    profile_picture VARCHAR(255),
    created_at datetime,
-   updated_at datetime
+   updated_at datetime,
+   INDEX idx_email (email)
 );
